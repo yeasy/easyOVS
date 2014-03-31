919066668877dad7b1230718ceaf137a81dc0d47
Baohua Yang <yangbaohua@gmail.com>
2014-03-31 13:54:30 +0800

    Check each field's existence before output them

--- bridge.py
+++ bridge.py
@@ -165,7 +165,12 @@ def brList():
     br_info = ''
     br_list = bridges.keys()
     for br in sorted(br_list):
-        br_info += "%s\n Port:\t\t%s\n Interface:\t%s\n Controller:\t%s\n Fail_Mode:\t%s\n"\
-                   %(br,' '.join(bridges[br]['Port']), ' '.join(bridges[br]['Interface']),\
-                     ' '.join(bridges[br]['Controller']),bridges[br]['fail_mode'])
+        if not bridges[br]['Port']:
+            br_info += "%s\n Port:\t\t%s\n"  %(br,' '.join(bridges[br]['Port']))
+        if not bridges[br]['Interface']:
+            br_info += " Interface:\t%s\n" %(' '.join(bridges[br]['Interface']))
+        if not bridges[br]['Controller']:
+            br_info += " Controller:\t%s\n"  %(' '.join(bridges[br]['Controller']))
+        if not bridges[br]['fail_mode']:
+            br_info += " Fail_Mode:\t%s\n"  %(bridges[br]['fail_mode'])
     return br_info
