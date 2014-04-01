b1af6f5a897f8b3994b7a3cd3f782d893e86fc1e
Baohua Yang <yangbaohua@gmail.com>
2014-04-01 17:28:08 +0800

    Optimize the neutron port list by enabling local cache defaultly (over timeout, will force to update).

--- easyovs/neutron.py
+++ easyovs/neutron.py
@@ -1,13 +1,25 @@
 __author__ = 'baohua'
 
+from os.path import exists,getmtime
 from subprocess import call,Popen,PIPE
+from time import  time
+import cPickle as pickle
 
-def get_neutron_ports():
+
+def get_neutron_ports(fresh=False):
         """
         Return the neutron port information, each line looks like
         qvoxxxx:{'id':id,'name':name,'mac':mac,"subnet_id": subnet_id, "ip_address": ip}
         """
         result={}
+        cache_file='/tmp/tmp_neutron_port'
+        if not fresh and exists(cache_file): #not quite fresh, then try to read local cache
+            if time()-getmtime(cache_file) < 60: #the cache is updated within 60s
+                try:
+                    result = pickle.load(open(cache_file, 'r'))
+                except Exception:
+                    return None
+                return result
         try:
             neutron_port_list= Popen('neutron port-list', stdout=PIPE,stderr=PIPE,shell=True).stdout.read()
         except Exception:
@@ -25,4 +37,6 @@ def get_neutron_ports():
                         id,name,mac,ips = l_value
                         result['qvo'+id[:11]] = {'id':id,'name':name,'mac':mac}
                         result['qvo'+id[:11]].update(eval(ips))
+        if result:
+            pickle.dump(result, open(cache_file, 'w'),True)
         return result
